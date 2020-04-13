be40d344cfc464ad318c326ddd3177d29536083f
Josezpt <63219390+Josezpt@users.noreply.github.com>
2020-04-13 17:36:03 -0500

    Update date.sh

--- date.sh
+++ date.sh
@@ -1,29 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/python
 
-#
-
-#
-
-CesarHackGray=$(mktemp)
-
-base64 -d  >${CesarHackGray}<<DIXIE
-
-Iy0tLS0tU1VCU0NSSUJFIE1ZIFlPVVRVQkUgRGluLXpVZ2V4OTUtLS0tLSMKCgpkYXRlPWBkYXRl
-
-ICIrJWQiYAptb250aD1gZGF0ZSAiKyVtImAKeWVhcnM9YGRhdGUgIisleSJgCgpkYXk9YGRhdGUg
-
-IislQSJgCgoKZWNobyAiICBcMDMzWzk1beKVvFwwMzNbOTRt4pi+IFwwMzNbOTNtREFURSBcMDMz
-
-Wzk1bTogXDAzM1s5Mm0kZGF0ZVwwMzNbOTVtL1wwMzNbOTJtJG1vbnRoXDAzM1s5NW0vXDAzM1s5
-
-Mm0keWVhcnMgXDAzM1s5NG3imL1cMDMzWzk1beKVvuKVvFwwMzNbOTRt4pi+IFwwMzNbOTNtREFZ
-
-IFwwMzNbOTVtOiBcMDMzWzkybSRkYXkgXDAzM1s5NG3imL1cMDMzWzk1beKVviIKZWNobwplY2hv
-
-Cg==
-
-DIXIE
-
-source ${CesarHackGray}
-
-rm -rf ${CesarHackGray}
+import base64
+Cesar = """Iy0tLS0tU1VCU0NSSUJFIE1ZIFlPVVRVQkUgRGluLXpVZ2V4OTUtLS0tLSMKCgpkYXRlPWBkYXRl ICIrJWQiYAptb250aD1gZGF0ZSAiKyVtImAKeWVhcnM9YGRhdGUgIisleSJgCgpkYXk9YGRhdGUg IislQSJgCgoKZWNobyAiICBcMDMzWzk1beKVvFwwMzNbOTRt4pi+IFwwMzNbOTNtREFURSBcMDMz Wzk1bTogXDAzM1s5Mm0kZGF0ZVwwMzNbOTVtL1wwMzNbOTJtJG1vbnRoXDAzM1s5NW0vXDAzM1s5 Mm0keWVhcnMgXDAzM1s5NG3imL1cMDMzWzk1beKVvuKVvFwwMzNbOTRt4pi+IFwwMzNbOTNtREFZ IFwwMzNbOTVtOiBcMDMzWzkybSRkYXkgXDAzM1s5NG3imL1cMDMzWzk1beKVviIKZWNobwplY2hv Cg=="""
+eval(compile(base64.b64decode(Cesar),'','exec'))
